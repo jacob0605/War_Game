b21a7dd52f17327562120db8d118086ab222c6b3
ShimYoungjun <jacob010605@gmail.com>
2025-03-03 08:04:35 +0900

    Add build  folder

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Git/War_Game
+# Build directory: C:/Git/War_Game/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
